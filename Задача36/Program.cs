1ae3810fa4af908c1b391add59b1c8aaf523509e
master <yarasmoothbeat@gmail.com>
2023-10-04 16:18:12 +0500

    36

---
+++ Задача36/Program.cs
@@ -0,0 +1,44 @@
+﻿int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    }
+
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+    }
+}
+
+void SumOfNumbersInOddPositions(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 == 0)
+        {
+            sum = sum + array[i];
+        }
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Сумма элементов на нечетных позициях - {sum} ");
+}
+
+System.Console.WriteLine("Input array size: ");
+int size = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input minimal value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input maximal value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] myArray = CreateRandomArray(size, min, max);
+PrintArray(myArray);
+SumOfNumbersInOddPositions(myArray);
