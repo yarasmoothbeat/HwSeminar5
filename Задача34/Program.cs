2abfcd3eaffb0555e5c939ebdd74aa7da2b83a85
master <yarasmoothbeat@gmail.com>
2023-10-04 15:21:27 +0500

    34

---
+++ Задача34/Program.cs
@@ -0,0 +1,38 @@
+﻿int[] CreateRandomArray(int size)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(100, 999 + 1);
+    }
+
+    return array;
+}
+
+void PrintArray(int[] array)
+
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+    }
+}
+
+void EvenNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Количество четных чисел - {count}" );
+}
+
+int[] myArray = CreateRandomArray(5);
+PrintArray(myArray);
+EvenNumbers(myArray);
